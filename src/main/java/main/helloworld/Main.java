250fa771ff364107e280fff2d2a726339649280f
Dragana Sindjelic <dragana9290@gmail.com>
2020-02-04 20:18:49 +0100

    Adds age checker

--- src/main/java/main/helloworld/Main.java
+++ src/main/java/main/helloworld/Main.java
@@ -1,5 +1,6 @@
 package main.helloworld;
 
+import java.util.Scanner;
 
 public class Main {
 
@@ -9,6 +10,42 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Dragana");
+
+        /*  int age = 5;
+
+        if (age > 18) {
+            System.out.println("Osoba je punoletna");
+        } else {
+            System.out.println("Osoba je maloletna");
+        }
+
+        System.out.print("Unesite broj godina.");
+        Scanner input = new Scanner(System.in);
+        int userAge = input.nextInt();
+
+        if (userAge > 18) {
+            System.out.println("Osoba je punoletna");
+        } else {
+            System.out.println("Osoba je maloletna");
+        }
+
+        System.out.println("Korisnik je uneo:" + userAge); */
+        System.out.print("Unesite broj godina.");
+        Scanner input2 = new Scanner(System.in);
+        int personAge = input2.nextInt();
+
+        if (personAge <= 0 && personAge > 150) {
+            System.out.println("Invalid");
+        } else if (personAge > 0 && personAge <= 5) {
+            System.out.println("Osoba je beba");
+        } else if (personAge <= 11) {
+            System.out.println("Osoba je dete");
+        } else if (personAge <= 17) {
+            System.out.println("Osoba je tinejdzer");
+        } else if (personAge >= 18) {
+            System.out.println("Osoba je punoletna");
+
+        }
     }
-    
+
 }
